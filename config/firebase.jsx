b52386b696b0be75ed28c6f7e747efb9336db4a3
Shahin <hawk.azizi1@gmail.com>
2023-09-04 13:54:21 +0330

    env file

--- config/firebase.jsx
+++ config/firebase.jsx
@@ -1,12 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import firebaseConfig from "@/env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnkPYqfpXlKTomIqQXqQx43SM4kA0eOdY",
+  authDomain: "noteapp-8153b.firebaseapp.com",
+  projectId: "noteapp-8153b",
+  storageBucket: "noteapp-8153b.appspot.com",
+  messagingSenderId: "560091074696",
+  appId: "1:560091074696:web:f8fd768141eb4d06984f3d",
+  measurementId: "G-H8559Z23R6",
+};
+
+export default firebaseConfig;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
